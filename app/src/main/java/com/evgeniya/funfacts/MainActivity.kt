a1c03d6da4e749fe7007f5d4104481e16a14de28
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-11-17 15:22:02 -0800

    made show fun facts random by clicking the button

--- app/src/main/java/com/evgeniya/funfacts/MainActivity.kt
+++ app/src/main/java/com/evgeniya/funfacts/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     //Declare out View variables
@@ -17,8 +18,23 @@ class MainActivity : AppCompatActivity() {
         factTextView = findViewById(R.id.factTextView)
         showFactButton = findViewById(R.id.showFactButton)
         showFactButton!!.setOnClickListener {
+            val facts= arrayOf(
+                "Ants stretch when they wake up in the morning.",
+                "Ostriches can run faster than horses.",
+                "Olympic gold medals are actually made mostly of silver.",
+                "You are born with 300 bones; by the time you are an adult you will have 206.",
+                "It takes about 8 minutes for light from the Sun to reach Earth.",
+                "Some bamboo plants can grow almost a meter in just one day.",
+                "The state of Florida is bigger than England.",
+                "Some penguins can leap 2-3 meters out of the water.",
+                "On average, it takes 66 days to form a new habit.",
+                "Mammoths still walked the earth when the Great Pyramid was being built.")
             //the button was clicked, so update the fact TextView with a new fact
-            val fact = "Ostriches can run faster than horses."
+            //Randomly select a fact
+            val randomGenerator = Random()
+            val randomNumber = randomGenerator.nextInt(facts.size)
+            val fact = facts[randomNumber]
+            //Update the screen with our new fact
             factTextView!!.text = fact
         }
     }
