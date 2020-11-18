9965baf944eb2ad48b8ad1da3732c555135b1780
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-11-17 18:41:58 -0800

    add functionality to change randomly fun facts and background color, and button text color, add some debug methods

--- app/src/main/java/com/evgeniya/funfacts/MainActivity.kt
+++ app/src/main/java/com/evgeniya/funfacts/MainActivity.kt
@@ -9,7 +9,10 @@ import android.widget.TextView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    //get class name
+    //private val TAG = MainActivity::class.java.simpleName
     private val factBook = FactBook()
+    private  val colorWheel = ColorWheel()
     //Declare out View variables
     private var factTextView: TextView? = null
     private var showFactButton: Button? = null
@@ -25,9 +28,15 @@ class MainActivity : AppCompatActivity() {
         relativeLayout = findViewById(R.id.relativeLayout)
         showFactButton!!.setOnClickListener {
             //Update the screen with our new fact
-            val fact = factBook.getFact();
+            val fact = factBook.getFact()
+            val color = colorWheel.getColor()
             factTextView!!.text = fact
-            relativeLayout!!.setBackgroundColor(Color.RED)
+            relativeLayout!!.setBackgroundColor(color)
+            showFactButton!!.setTextColor(color)
         }
+
+        //Testing
+        //Toast.makeText(this, "Info message", Toast.LENGTH_LONG).show()
+        //Log.d(TAG, "Info message")
     }
 }
