86ee048f1a859897ed001f170ebb2a79730385f1
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-11-17 17:32:55 -0800

    create FunFacts and ColorWheel classes to refuctor the code

--- app/src/main/java/com/evgeniya/funfacts/MainActivity.kt
+++ app/src/main/java/com/evgeniya/funfacts/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.evgeniya.funfacts
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.RelativeLayout
 import android.widget.TextView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private val factBook = FactBook()
     //Declare out View variables
     private var factTextView: TextView? = null
     private var showFactButton: Button? = null
+    private var relativeLayout: RelativeLayout? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,25 +22,12 @@ class MainActivity : AppCompatActivity() {
         //assign the Views from the layout file to the corresponding variables
         factTextView = findViewById(R.id.factTextView)
         showFactButton = findViewById(R.id.showFactButton)
+        relativeLayout = findViewById(R.id.relativeLayout)
         showFactButton!!.setOnClickListener {
-            val facts= arrayOf(
-                "Ants stretch when they wake up in the morning.",
-                "Ostriches can run faster than horses.",
-                "Olympic gold medals are actually made mostly of silver.",
-                "You are born with 300 bones; by the time you are an adult you will have 206.",
-                "It takes about 8 minutes for light from the Sun to reach Earth.",
-                "Some bamboo plants can grow almost a meter in just one day.",
-                "The state of Florida is bigger than England.",
-                "Some penguins can leap 2-3 meters out of the water.",
-                "On average, it takes 66 days to form a new habit.",
-                "Mammoths still walked the earth when the Great Pyramid was being built.")
-            //the button was clicked, so update the fact TextView with a new fact
-            //Randomly select a fact
-            val randomGenerator = Random()
-            val randomNumber = randomGenerator.nextInt(facts.size)
-            val fact = facts[randomNumber]
             //Update the screen with our new fact
+            val fact = factBook.getFact();
             factTextView!!.text = fact
+            relativeLayout!!.setBackgroundColor(Color.RED)
         }
     }
 }
